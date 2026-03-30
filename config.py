8147e64062617515249f887dfcd446c33ccbfeb2
Vitor Furuta <vitorfurut4@gmail.com>
2026-03-30 18:01:56 -0300

    Rename data file from users_data.json to data.json

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ MIN_TICK_TIME = 1 # Tempo mínimo para alteração de preço
 MAX_TICK_TIME = 3 # Tempo máximo para alteração de preço
 FEED_INTERVAL = 5 # Feed constante de preços que o servidor vai mandar pro cliente
 MIN_PRICE = 0.01 # Menor valor possível
-DATA_FILE = "users_data.json"
+DATA_FILE = "data.json"
 TIMEOUT_TIME = 360 #segundos para timeout 
 
 
